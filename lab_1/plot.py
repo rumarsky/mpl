7d4dabdecb801956d710e5d4106d19d578f90367
glebmlakir <glebmlakir.one@gmail.com>
2025-10-04 11:06:54 +1000

    Bug fix

--- lab_1/plot.py
+++ lab_1/plot.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_genre_ratings(genre_to_avg, save_path=None):
+def plot_genre_ratings(genre_to_avg):
     genres = list(genre_to_avg.keys())
     avgs = list(genre_to_avg.values())
 
@@ -10,18 +10,15 @@ def plot_genre_ratings(genre_to_avg, save_path=None):
     plt.title("Средний рейтинг по жанрам (чем выше, тем лучше)")
     plt.xlabel("Жанр")
     plt.ylabel("Средний рейтинг")
-    plt.grid(axis="y", linestyle="--", alpha=0.4)
+    plt.grid(axis="y", linestyle="--")
 
     plt.xticks(rotation=20)
 
     for bar, value in zip(bars, avgs):
         height = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width()/2, height + 0.02, str(value),
+        plt.text(bar.get_x() + bar.get_width()/2, height, str(value),
                  ha='center', va='bottom')
 
     plt.tight_layout()
-
-    if save_path:
-        plt.savefig(save_path, dpi=150)
 
     plt.show()
